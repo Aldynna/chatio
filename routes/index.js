0fe5d1a952e2b3348c159d5c4c62470bd2618edb
Aldynna <aldina.j90@gmail.com>
2017-11-15 23:13:34 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var alert=require('alert-node');
 var createStream = fs.createWriteStream("chat2.txt");
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,21 +17,25 @@ router.get('/chat', function(req, res, next) {
        // console.log(buf.toString());
         poruke=buf.toString();
         por=buf;
-      //  console.log(poruke);
+     console.log(poruke);
+     alert(' '+poruke);
+
 
 
     });
   res.render('index', { title: 'Chat', poruke:por,nick:nick});
 });
 
-router.get('/text', function(req, res, next) {
+router.post('/text', function(req, res, next) {
     var por;
     var poruke;
     fs.readFile('chat.txt','utf8', function(err, buf) {
        // console.log(buf.toString());
         poruke=buf.toString();
         por=buf;
-        //console.log(poruke);
+
+     alert(' '+poruke);
+       console.log(poruke);
         poruke="nesto"
 
     });
@@ -47,13 +52,15 @@ router.get('/text', function(req, res, next) {
 router.post('/poruka', function(req, res, next) {
     //console.log(req);
     //res.send(req.body.fname);
+    alert(req.cookies.nick+' : '+req.body.fpor+'\n');
     fs.appendFile('chat.txt', req.cookies.nick+" : "+req.body.fpor+"\n", function (err) {
         if (err) return console.log(err);
 
 
-        console.log('pisemo u txt',req.cookies.nick+" : "+req.body.fpor);
+        console.log('pisemo u txtpor',req.cookies.nick+" : "+req.body.fpor);
 
     });
+
 res.redirect('/chat?nick='+req.cookies.nick);
 
 });
@@ -62,12 +69,14 @@ res.redirect('/chat?nick='+req.cookies.nick);
 router.post('/login', function(req, res, next) {
     //console.log(req);
     //res.send(req.body.fname);
+    alert(req.body.nick+' se pridruzio!\n');
     res.cookie('nick', req.body.nick,{expire : new Date() + 9999});
     fs.appendFile('chat.txt', req.body.nick+" se pridruzio!\n", function (err) {
         if (err) return console.log(err);
         console.log('pisemo u txt',req.body.nick+ +" se pridruzio!\n");
 
     });
+
     res.redirect('/chat?nick='+req.body.nick);
 
 });
